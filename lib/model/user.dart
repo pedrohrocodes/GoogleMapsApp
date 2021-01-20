952d8f0302f71efdf06c4974cb8008c69091f96d
Pedro Henrique Rocha <pedrohrocodes@gmail.com>
2021-01-20 11:52:41 -0300

    :sparkles: Adding User Class

---
+++ lib/model/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String username;
+  final String name;
+  final String image;
+  // final LatLng location;
+  final int rating;
+
+  User(this.username, this.name, this.image, this.rating);
+}
